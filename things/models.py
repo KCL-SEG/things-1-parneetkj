2931e9fc9751a20ac8a7fba5e0ac58a7902aabeb
Parneet Johal <parneet.johal@kcl.ac.uk>
2022-10-08 18:29:54 +0100

    Fix Thing parameters

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(name, description, quantity):
+class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
